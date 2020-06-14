8a95cddaeb22b71001a9887b80d6cc62e9dbd9d6
lkxglobal <315@lkx.com>
2020-06-14 17:18:12 +0800

    cat提交

---
+++ src/extend/Cat.java
@@ -0,0 +1,11 @@
+package extend;
+
+
+public  class Cat {
+	
+	String name="√®œ≤ª∂≥‘”„";
+	public void cat() {
+		System.out.println("naem");
+	}
+	
+}
---
+++ src/extend/Cats.java
@@ -0,0 +1,19 @@
+package extend;
+
+
+public class Cats extends Cat {
+	String name="√®œ≤ª∂≥‘”„111111111111";
+	public void cat() {
+		//super.cat();
+		System.out.println("");
+	}
+
+	public static void main(String[] args) {
+
+		Cats ca = new Cats();
+		ca.cat();
+	
+	}
+
+	
+}
